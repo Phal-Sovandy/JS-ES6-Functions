7dc6916b4f4961341f0898279b1bc7c1d76fffc1
Phal Sovandy <sovandy.phal@student.cadt.edu.kh>
2025-02-13 14:17:13 +0700

    Fix index calculation in updateStudentGrade function

--- ex5 - Clone and update with spread.js
+++ ex5 - Clone and update with spread.js
@@ -17,7 +17,7 @@ let students = [
 function updateStudentGrade(studentsArray, idToUpdate, newGrade) {
   // Step 1 : Use the findIndex method to find the index of the student with the provided ID in the studentsArray.
   let indexToUpdate = -1; // Changeable varible should not declare using 'const' keyword
-  indexToUpdate = studentsArray.findIndex(student => student.id === idToUpdate) ? 0 : -1;
+  indexToUpdate = studentsArray.findIndex(student => student.id === idToUpdate);
 
   if (indexToUpdate !== -1) {
     // Step 2 : If the student with the provided ID is found , create a copy of the original array using the spread operator.
